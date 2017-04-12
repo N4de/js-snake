73c944d34b487bbfe2f4590a7d3d4690fe1e0f4b
Jari Annala <Jari Annala>
2017-04-12 17:50:03 +0300

    food, gameover

--- snake.js
+++ snake.js
@@ -9,7 +9,8 @@ $(document).ready(function(){
 
 
 // GLOBAL VARIABLES
-var start = setInterval(function() {snake.move();},1000);
+var start = setInterval(function() {snake.move();},500);
+
 
 gameOver = function() {
 	$('p').removeClass('hidden');
@@ -22,6 +23,8 @@ var grid = {
 	defaultBox: " ",
 	boxCoordinates: [],
 	size: 40,
+	isFood: false,
+	foodCoords: [],
 
 	render: function() {
 		for (var i = 0; i < 40; i++) {
@@ -33,9 +36,20 @@ var grid = {
 	},
 
 	spawnFood: function() {
-		var x = Math.floor((Math.random() * 39));
-		var y = Math.floor((Math.random() * 39));
-		$("#box-" + x + "-" + y).addClass("food");
+		if (grid.isFood === false) {
+			var x = Math.floor((Math.random() * 39));
+			var y = Math.floor((Math.random() * 39));
+			$("#box-" + x + "-" + y).addClass("food");
+			grid.isFood = true;
+			grid.foodCoords.push(x);
+			grid.foodCoords.push(y);
+		}
+	},
+
+	removeFood: function() {
+		$("#box-" + grid.foodCoords[0] + "-" + grid.foodCoords[1]).removeClass("food");
+		grid.isFood = false;
+		grid.foodCoords = [];
 	}
 }
 
@@ -46,6 +60,7 @@ var snake = {
 	partX: [20, 20],
 	partY: [18, 19],
 	direction: "right",
+	foodEaten: false,
 
 
 	render: function() {
@@ -57,32 +72,27 @@ var snake = {
 
 	changeDirection: function() {
 		$('body').keydown(function( event ) {
-			console.log("keydown called");
 	      	event.preventDefault();
 	      	switch(event.which) {
 		        case 37:
 		        	if (snake.direction != "right") {
 		        		snake.direction = ("left");
 		        	}
-		        	console.log("left pressed");
 		          	break;
 		        case 38:
 		        	if (snake.direction != "down") {
 		          		snake.direction = ("up");
 		        	}
-		          	console.log("up pressed");
 		          	break;
 		        case 39:
 		        	if (snake.direction != "left") {
 		          		snake.direction = ("right");
 		          	}
-		          	console.log("right pressed");
 		          	break;
 		        case 40:
 		        	if (snake.direction != "up") {
 		          		snake.direction = ("down");
 		          	}
-		          	console.log("down pressed");
 		          	break;
 		        default: break; 
 	    	}
@@ -93,9 +103,13 @@ var snake = {
 		// move snake head one space, move the first body to head, remove last body
 		$("#box-" + snake.headX + "-" + snake.headY).removeClass("snake-head");
 		$("#box-" + snake.headX + "-" + snake.headY).addClass("snake-body");
-		$("#box-" + snake.partX[0] + "-" + snake.partY[0]).removeClass("snake-body");
-		snake.partX.shift();
-		snake.partY.shift();
+		if (snake.foodEaten === false) {
+			$("#box-" + snake.partX[0] + "-" + snake.partY[0]).removeClass("snake-body");
+			snake.partX.shift();
+			snake.partY.shift();
+		}
+		snake.foodEaten = false;
+
 		snake.partX.push(snake.headX);
 		snake.partY.push(snake.headY);
 
@@ -116,13 +130,26 @@ var snake = {
 				break;
 		}
 		$("#box-" + snake.headX + "-" + snake.headY).addClass("snake-head");
-		console.log("bodyX: " + snake.partX);
-		console.log("bodyY: " + snake.partY);
-		console.log("headX: " + snake.headX);
-		console.log("headY: " + snake.headY);
 
+		snake.lose();
+
+		grid.spawnFood();
+
+		if (grid.foodCoords[0] === snake.headX && grid.foodCoords[1] === snake.headY) {
+			snake.foodEaten = true;
+			grid.removeFood();
+		}
+	},
+
+	lose: function() {
 		if (snake.headX === 40 || snake.headY === 40 || snake.headX === -1 || snake.headY === -1) {
 			gameOver();
+		}
+
+		for (var i = 0; i < snake.partX.length; i++) {
+			if (snake.headX === snake.partX[i] && snake.headY === snake.partY[i]) {
+				gameOver();
+			}
 		}
 	}
 }
